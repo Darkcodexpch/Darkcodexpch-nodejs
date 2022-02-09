99e68383e68a3401de24a9291431ccf55c073b6b
Darkcodexpch <ikamihere@gmail.com>
2022-02-09 17:53:39 +0500

    mongo db connection with server

--- index.mjs
+++ index.mjs
@@ -1,7 +1,11 @@
-import express from 'express'
+import express,{response} from 'express'
+import mongoose from 'mongoose'
+import cors from "cors"
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(express.json()) //use for parse data in json format 
+
+app.use(express.json()) //use for parse data in json format
+app.use(cors()); 
 
 
 let posts = [
@@ -9,6 +13,34 @@ let posts = [
   { text: "Ali" },
   { text: "Basit" }
 ]
+// mongodb connect
+let dbURI = 'mongodb+srv://dark:dark@cluster0.odg9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+  console.log("Mongoose is connected");
+  // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+  console.log("Mongoose is disconnected");
+  process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+  console.log('Mongoose connection error: ', err);
+  process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+  console.log("app is terminating");
+  mongoose.connection.close(function () {
+      console.log('Mongoose default connection closed');
+      process.exit(0);
+  });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
 
 app.get('/', (req, res) => {
   res.send('hello this is express js server and kamran here!')
@@ -28,7 +60,7 @@ app.get('/post:id', (req, res) => {
 // for add post data to server
 app.post('/posts', (req, res) => {
   posts.push(req.body);
-  res.send(`Your Post is saved now and now we have ${posts.length} posts`)
+  res.send(`Your post is saved now and now ❤  we have ${posts.length} posts`)
 })
 
 // for updating record
@@ -43,5 +75,5 @@ app.delete('/post', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`app listening at ${PORT}`)
 })
+  console.log(`app listening at ${PORT}`)
